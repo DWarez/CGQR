44d6f4ef0f82a8ecbfde6e7ccb9094ddfcf7ba5d
DWarez <ldwarezl@gmail.com>
2022-04-02 18:43:12 +0200

    small change

--- src/conjugate_gradient.cpp
+++ src/conjugate_gradient.cpp
@@ -10,11 +10,13 @@ arma::vec conjugate_gradient(const arma::mat &X, const arma::vec &b, uint n_iter
     arma::vec residual(b);
     arma::vec direction(b);
     arma::vec previous_residual(residual);
-    arma::vec w(b.n_elem, arma::fill::zeros);
+    arma::vec w(b.n_elem, arma::fill::ones);
     std::vector<double> history;
 
     auto start = std::chrono::steady_clock::now();
     std::cout << "Starting iterations\n";
+    // starting residual
+    history.push_back(arma::norm(X*w - b)/arma::norm(b));
 
     for(size_t i = 0; i < n_iterations; ++i) {
         auto alpha = (((arma::dot(residual.t(), residual))/(direction.t() * X * direction)).eval())(0, 0);
