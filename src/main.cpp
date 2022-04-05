dca31d3c1e3f2c891b5b1501439623d1bf13b096
DWarez <ldwarezl@gmail.com>
2022-04-05 16:30:31 +0200

    Fixed to_normal_equations()

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <sys/stat.h>
 #include "../include/utils.hpp"
 #include "../include/conjugate_gradient.hpp"
+#include "../include/qr_factorization.hpp"
 
 void cg_experiment() {
 
@@ -18,10 +19,26 @@ void cg_experiment() {
     // solution vector
     arma::vec w = arma::vec(X.n_cols, arma::fill::zeros);
 
-    w = conjugate_gradient(X, b, X.n_cols);
+    w = conjugate_gradient(X, b, 1000);
+}
+
+void qr_experiment() {
+    arma::mat X, Q, R;
+    arma::vec b;
+    
+    std::tie(X, b) = grab_mlcup_dataset();
+    arma::vec w(X.n_cols, arma::fill::zeros);
+    std::tie(Q, R) = thin_qr(X);
+
+    std::cout << "Norm before: " << arma::norm(X*w - b)/arma::norm(b) << std::endl;
+
+    w = solve_thin_qr(Q, R, b);
+
+    std::cout << "Norm after: " << arma::norm(X*w - b)/arma::norm(b) << std::endl;
 }
 
 int main(int argc, char** argv) {
-    cg_experiment();
+    // cg_experiment();
+    qr_experiment();
     return 0;
 }
