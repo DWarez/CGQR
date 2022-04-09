6ae3747a5b9ada7dcc7fc812e268c9a8f2bb2452
DWarez <ldwarezl@gmail.com>
2022-04-09 17:43:49 +0200

    Added distance from optimal solution in experiments
    We now find the optimal solution using arma.solve() and show the
    distance between the obtained result and the optimal one.

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,13 @@ void cg_experiment() {
     std::tie(n_X, n_b) = to_normal_equations(X, b);
 
     // solution vector
+    arma::vec solution = arma::solve(X, b);
     arma::vec w = arma::vec(X.n_cols, arma::fill::zeros);
 
-    w = conjugate_gradient(n_X, n_b, 400);
+    w = conjugate_gradient(n_X, n_b, 10000);
 
     std::cout << "Norm of CG: " << arma::norm(X*w - b) << std::endl;
+    std::cout << "Distance from optimal solution: " << arma::norm(w - solution) << std::endl;
 
     std::cout << "==========================\nEnd of the CG experiment" << std::endl;
 }
@@ -34,17 +36,20 @@ void qr_experiment() {
 
     std::tie(X, b) = grab_mlcup_dataset();
     add_columns(X);
+    arma::vec solution = arma::solve(X, b);
+
     arma::vec w(X.n_cols, arma::fill::zeros);
     std::tie(Q, R) = thin_qr(X, b);
 
     w = solve_thin_qr(Q, R);
 
     std::cout << "Norm of QR: " << arma::norm(X*w - b) << std::endl;
+    std::cout << "Distance from optimal solution: " << arma::norm(w - solution) << std::endl;
     std::cout << "==========================\nEnd of the QR experiment" << std::endl;
 }
 
 int main(int argc, char** argv) {
-    // cg_experiment();
+    cg_experiment();
     qr_experiment();
     return 0;
 }
